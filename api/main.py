a975e33f29061758b57829201823e483a7a932ab
Seydina Oumar Diallo <dialloseydinaoumar893@gmail.com>
2026-05-10 21:31:30 +0000

    Ajout exos du lab 3

--- api/main.py
+++ api/main.py
@@ -103,6 +103,18 @@ def health_check():
         "status": "ok",
         "message": "SenSante API is running"
     }
+
+# ===== Route model-info =====
+
+@app.get("/model-info")
+def model_info():
+
+    return {
+        "type_modele": type(model).__name__,
+        "nombre_arbres": model.n_estimators,
+        "classes": list(model.classes_),
+        "nombre_features": model.n_features_in_
+    }
 	
 # ===== Route predict =====
 
